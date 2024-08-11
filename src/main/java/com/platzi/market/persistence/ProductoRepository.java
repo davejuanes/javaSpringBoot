0a7bfe5fe18e7a22017e73ffdc35208dce4ed262
Dave Juanes <dmercado@abc.gob.bo>
2024-08-10 22:29:01 -0400

    [NEW] QUERY METHODS

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -4,11 +4,18 @@ import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public Iterable<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc((idCategoria));
+    }
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
